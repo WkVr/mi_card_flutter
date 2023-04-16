f171148bcd83ef4b128c92a0aa177e99a0a89e37
Wynand van Rensburg <wkvanrensburg@gmail.com>
2023-04-16 12:39:50 +0200

    Add email text

--- lib/main.dart
+++ lib/main.dart
@@ -37,6 +37,56 @@ class MyApp extends StatelessWidget {
                   letterSpacing: 2.5,
                   fontWeight: FontWeight.bold,
                 ),
+              ),
+              Container(
+                color: Colors.white,
+                margin: const EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 20,
+                ),
+                padding: const EdgeInsets.all(10),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal.shade900,
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      '+27 123 456 789',
+                      style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontSize: 20,
+                          fontFamily: 'SansSourcePro'),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                margin: const EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 20,
+                ),
+                padding: const EdgeInsets.all(10),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal.shade900,
+                    ),
+                    const SizedBox(width: 10),
+                    Text(
+                      'wynand@email.com',
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontFamily: 'SansSourcePro',
+                          color: Colors.teal.shade900),
+                    )
+                  ],
+                ),
               )
             ],
           ),
