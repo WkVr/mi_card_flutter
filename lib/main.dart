1278f62e386f37cc63b01706d46dcc20d167f5ac
Wynand van Rensburg <wkvanrensburg@gmail.com>
2023-04-16 12:22:57 +0200

    Add Text and custom styling

--- lib/main.dart
+++ lib/main.dart
@@ -14,19 +14,29 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
-            children: const [
-              CircleAvatar(
+            children: [
+              const CircleAvatar(
                 radius: 50,
                 backgroundColor: Colors.blueGrey,
                 backgroundImage: AssetImage('images/person.png'),
               ),
-              Text(
+              const Text(
                 'Wynand',
                 style: TextStyle(
                     fontSize: 40,
                     color: Colors.white,
                     fontWeight: FontWeight.bold,
                     fontFamily: 'Pacifico'),
+              ),
+              Text(
+                'SOFTWARE DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  color: Colors.teal[100],
+                  fontSize: 20,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
               )
             ],
           ),
